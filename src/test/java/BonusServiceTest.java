2f01dc2649854e6fcfc13fd1dfe484513b821246
NikitaMak234 <makarovnikita020395@yandex.ru>
2022-02-07 15:32:29 +0400

    Init

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -33,6 +33,36 @@ class BonusServiceTest {
 
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
+    }@Test
+    void shouldCalculateForUnregisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldCalculateForUnregisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
+
